8d1f118fc8e19f29562b02138175584b25ff1f67
maserrasm <maserra@miuandes.cl>
2020-04-27 16:23:05 -0400

    Lab Manuel Serra 0.1

---
+++ Desktop/laboratorio-05-maserrasm/Solucion_Lab_21_abril/MailSender.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Solucion_Lab_21_abril
+{
+
+
+    public class MailSender
+    {
+        public delegate void OnEmailSent(object sender, EventArgs e );
+        public event OnEmailSent OnEmailSentEvent; 
+
+        public void OnRegistered(object source, RegisterEventArgs e)
+        {
+            Thread.Sleep(2000);
+            Console.WriteLine($"\nCorreo enviado a {e.Email}: \n Gracias por registrarte, {e.Username}!\n Por favor, para poder verificar tu correo, has click en: {e.VerificationLink}\n");
+            Thread.Sleep(2000);
+
+            if (OnEmailSentEvent != null) 
+                OnEmailSentEvent(this, null);
+                Console.WriteLine("Se dispara evento onEmailSent.");
+
+        }
+
+        public void OnPasswordChanged(object source, ChangePasswordEventArgs e)
+        {
+            Thread.Sleep(2000);
+            Console.WriteLine($"\nCorreo enviado a {e.Email}:  \n {e.Username}, te notificamos que la contrasena de tu cuenta PlusCorp ha sido cambiada. \n");
+            Thread.Sleep(2000);
+        }
+
+    }
+}
---
+++ Desktop/laboratorio-05-maserrasm/Solucion_Lab_21_abril/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solucion_Lab_21_abril
+{
+    class User
+    {
+    }
+}
